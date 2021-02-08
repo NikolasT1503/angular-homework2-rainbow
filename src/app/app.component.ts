b034609be7ca1ce0263cb1c2256507ef3d6db886
NikolasT1503 <Nikolas_t@mail.ru>
2021-02-09 00:39:01 +0300

    add interpolation, data binding, event on button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-homework2-rainbow';
+  red = 'rgb(255,0,0)';
+  orange = 'rgb(255,91,0)';
+  yellow = 'rgb(255,255,0)';
+  green = 'background-color: rgb(0,255,0)';
+  deepskyblue = 'background-color: rgb(128,166,255)';
+  blue = 'background-color: rgb(0,0,255)';
+  purple = 'background-color: rgb(128,0,255)';
+
+  classParam: string = 'visible';
+
+  hided: boolean = false;
+
+  @Input() 
+  inputParam:string;
+
+  @Output()
+  userChange = new EventEmitter<string>();
+
+  hideRows(e : any): void{
+    this.classParam = e.target?.value||e.value;
+    console.log('Событие пришло: ', this.classParam);
+    this.classParam = this.hided ? 'visible' : 'collapse';
+    console.log('Установка classParam: ', this.classParam);
+
+    this.hided = this.hided ? false : true;
+    this.userChange.emit(this.classParam);
+  }
+
+
 }
